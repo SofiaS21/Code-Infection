d08ccd7570821f6bd8f184684368e42b4b5410d9
SofiaS21 <49876490@est.ort.edu.ar>
2026-08-05 14:26:51 -0300

    Agregando pastillas

---
+++ Assets/Scripts/RecolectarObjeto.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecolectarObjeto : MonoBehaviour
+{
+      private Camera cam;
+
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    void LateUpdate()
+    {
+        transform.LookAt(transform.position + cam.transform.rotation * Vector3.forward,     cam.transform.rotation * Vector3.up);
+    }
+}
