8f531c55de941f502ae0a25e76237d809a40e096
SofiaS21 <49876490@est.ort.edu.ar>
2026-08-10 14:22:30 -0300

    Cambios minimos

---
+++ Assets/Scripts/Main menu/UIManagerMM.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManagerMM : MonoBehaviour
+{
+
+    public string iniciarSesion
+    public string opciones
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
